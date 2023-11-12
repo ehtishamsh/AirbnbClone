7c81ac486d4092f772d19278c04cafcc4a93faf3
Ehtisham <117318809+ehtishamsh@users.noreply.github.com>
2023-11-13 01:37:03 +0500

    image

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo from "./assets/logo.svg";
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="" alt="" />
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
